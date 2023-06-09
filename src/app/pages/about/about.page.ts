d76d17067d37f2d6ece2a298482db23a03bf2978
Hanzlah10 <hanzalasarguroh@gmail.com>
2023-06-09 10:31:36 +0530

    new commit

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
-
+  imageUrl:string='../assets/me.png'
   constructor() { }
 
   ngOnInit() {
